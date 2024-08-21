ce1411692c165bfeb08fd22eb6b7ded935aec56f
nayanrashed <rashednayan13@gamil.com>
2024-08-22 01:59:45 +0600

    something about list

--- python_basics_02/list_exp2.py
+++ python_basics_02/list_exp2.py
@@ -1,3 +1,32 @@
+#List nad Nested List
+p=[1, 2, 3, 4, 5]
+print(p[1])
+
+x=[[1,2,3],[4,5,6]]
+print(x[1][1])
+
+
 # Negetive Index
 p=[1, 2, 3, 4, 5]
 print(p[-1])
+
+
+# Sub Lists and Slicing
+q=[1,2,3,4,5,6]
+print(q[2:6])
+print(q[:5])
+print(q[:])
+#q[start_index: end_index+1]
+
+a=[1,2,3,4]
+b=['x','y','z']
+ab=a+b
+print(ab)
+c=['p','q','r']
+new_c=c*2
+print(new_c)
+
+
+#delete in List
+del c[0]
+print(c)
