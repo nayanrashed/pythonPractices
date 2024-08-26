b921b32ba07bc05e18068f11210abe74a8f65e42
nayanrashed <rashednayan13@gamil.com>
2024-08-27 03:11:15 +0600

    dickies

---
+++ python_basics_02/dictionary_method.py
@@ -0,0 +1,38 @@
+identity = {'Name':'Sabbir', 'Age':'21', 'Job':'Student'}
+
+'''#Printing values
+for i in identity.values():
+    print(i)
+    
+#Printing keys
+for i in identity.keys():
+    print(i)
+    
+#Printing keys+values
+for i in identity.items():
+    print(i)'''
+
+'''X=list(identity.keys())
+Y=list(identity.values())
+print(X)
+print(Y)'''
+
+'''# A HANDY TRICK
+for k,v in identity.items():
+    print('key: '+k+' value: '+v)
+'''
+# use of 'in' keyword
+'''print('Name' in identity)
+print('name' in identity)
+print('nayan' in identity.values())
+print('Age' in identity.keys())'''
+
+# the get() method
+'''print(str(identity.get('Name', 'Default')))
+print(str(identity.get('Height', 'Default')))'''
+
+
+# set default
+print(identity.setdefault('Name', 'Cosmos'))
+print(identity.setdefault('Height','6ft'))
+print(identity)
