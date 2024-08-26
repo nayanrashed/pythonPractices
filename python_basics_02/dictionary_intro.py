ea37b78c1fc6818141c45f043867d1abd22bf8a9
nayanrashed <rashednayan13@gamil.com>
2024-08-27 02:45:48 +0600

    dictionary in python

---
+++ python_basics_02/dictionary_intro.py
@@ -0,0 +1,19 @@
+'''my_stuff = {'book':'Cookbook', 'phone':'OnePlus', 'Home':'Bangladesh'}
+print(my_stuff['book'])
+x={0:0, 1:100, 2:200}
+print(x[1])
+# Order in Dictionary 
+a=[1,2,3]
+b=[2,3,1]
+print(a==b)
+
+c = {1:10, 2:20}
+d ={ 2:20, 1:10}
+print(c==d)'''
+
+# concatenation of dictionaries
+D={'a':100,'b':200}
+E={'c':300,'d':400}
+new_dic=D.copy()
+new_dic.update(E)
+print(new_dic)
