7d56ce1aaa62debf058fe333b10de55066b939a0
nayanrashed <rashednayan13@gamil.com>
2024-09-02 19:26:43 +0600

    dictionary problems

---
+++ problems-solutions/letter_counting.py
@@ -0,0 +1,7 @@
+import pprint as pp
+text = 'i love my country'
+letters = {}
+for i in text:
+    letters.setdefault(i,0)
+    letters[i]=letters[i]+1
+pp.pprint(letters)
---
+++ python_basics_02/dictionary_example.py
@@ -0,0 +1,10 @@
+#letter counting
+'''import pprint as pp
+text = 'i love my country'
+letters = {}
+for i in text:
+    letters.setdefault(i,0)
+    letters[i]=letters[i]+1
+pp.pprint(letters)'''
+
+#Password checking/controlling
